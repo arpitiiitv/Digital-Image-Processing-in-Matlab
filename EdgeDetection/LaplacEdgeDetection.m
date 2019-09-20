4bd42c66bd63ae041ba08649ee9295ec5a3918d9
Arpit tiwari <38579437+arpitiiitv@users.noreply.github.com>
2019-09-20 22:57:16 +0530

    horizontal and vertical edge detected

--- EdgeDetection/LaplacEdgeDetection.m
+++ EdgeDetection/LaplacEdgeDetection.m
@@ -1,15 +1,27 @@
 %% Edge detection using Laplace Impulse response
 img=im2double(imread("myImage.jpg"));
 whos img
-subplot(1,2,1)
+subplot(2,2,1)
 imshow(img)
 title("Real Image")
 %% Laplace impulse  response function
+h1=[1,-2,1];
+h2=[1;-2;1];
 h=[0,1,0;1,-4,1;0,1,0];
 %% convolution with real image
 edgeImg=conv2(img,h);
-subplot(1,2,2)
+subplot(2,2,2)
 imshow(edgeImg)
 title("Laplace Edge")
+%% vertical edge
+VEdgeImg=conv2(img,h1);
+subplot(2,2,4)
+imshow(VEdgeImg)
+title("Laplace Edge")
 
+%% horizontal edge
+HEdgeImg=conv2(img,h2);
+subplot(2,2,3)
+imshow(HEdgeImg)
+title("Laplace Edge")
 sgtitle("Edge Detection Usng Laplace Methode")
